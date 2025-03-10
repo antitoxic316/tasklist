bfe8322a153d445344400df70f9b0caac5a4b374
antitoxic336 <73442802+antitoxic316@users.noreply.github.com>
2025-03-10 21:46:14 +0100

    fixed callbacks structure in mainwindow.c

--- gui/mainwindow.c
+++ gui/mainwindow.c
@@ -28,26 +28,51 @@ void activate_mainwindow(GtkApplication *app, gpointer user_data){
 
   tasksBoxList = gtk_builder_get_object(builder, "taskBoxList");
 
-  g_signal_connect_swapped(newTaskButton, "clicked", G_CALLBACK(add_task_handler), tasksBoxList);
+  g_signal_connect_swapped(newTaskButton, "clicked", G_CALLBACK(task_init_dialog), window);
   g_signal_connect(removeTaskButton, "clicked", G_CALLBACK(remove_task_handler), NULL);
 
   gtk_widget_set_visible (GTK_WIDGET (window), TRUE);
   g_object_unref (builder);
 }
 
-
-void add_task_handler(GtkWidget *taskboxlist){
-    Task *new_task;
-
-    GtkRoot *root = gtk_widget_get_root (GTK_WIDGET (taskboxlist));
-    GtkWindow *window = GTK_WINDOW (root);
-
-    new_task = task_init_dialog(window);
-    tasklist_serializer_file_append(new_task);
-    taskboxlist_show_task(new_task, taskboxlist);
+void dialog_response_handler(GtkDialog *dialog, gint response_id){
+  if(response_id == GTK_RESPONSE_ACCEPT){
+    dialog_accept_handler(dialog);
+  } else if(response_id == GTK_RESPONSE_REJECT) {
+    dialog_reject_handler(dialog);
+  }
 }
 
-void taskboxlist_show_task(Task *task, GtkWidget *taskboxlist){
+void dialog_reject_handler(GtkDialog *dialog){
+
+}
+
+void dialog_accept_handler(GtkDialog *dialog){
+    GtkRoot *root = gtk_widget_get_root (GTK_WIDGET (dialog));
+    GtkWindow *window = GTK_WINDOW (root);
+    GObject *taskboxlist = gtk_widget_get_template_child(GTK_WIDGET(window), GTK_TYPE_WINDOW, "taskBoxList");
+
+    GObject *task_name_entry, *task_deadline_entry;
+
+    Task *task;
+    char *task_name;
+    time_t task_deadline = 0;
+
+    task_name_entry = gtk_widget_get_template_child(GTK_WIDGET(dialog), GTK_TYPE_DIALOG, "taskNameInput");
+    task_deadline_entry = gtk_widget_get_template_child(GTK_WIDGET(dialog), GTK_TYPE_DIALOG, "taskDeadlineInput");
+
+    GtkEntryBuffer *task_name_entry_buffer = gtk_entry_get_buffer(GTK_ENTRY(task_name_entry));
+    task_name = gtk_entry_buffer_get_text(task_name_entry_buffer);
+
+    task = task_init(task_name, task_deadline);
+
+    taskboxlist_show_task(GTK_WIDGET(taskboxlist), task);
+
+    free(task);
+    return;
+}
+
+void taskboxlist_show_task(GtkWidget *taskboxlist, Task *task){
 
 }
 
@@ -55,39 +80,36 @@ void remove_task_handler(){
 
 }
 
-Task *task_init_dialog(GtkWindow *parent){
-    Task *task;
-
-    char *task_name = "test";
-    time_t task_deadline = 5;
-
-    GtkWidget *dialog, *label, *content_area;
+void task_init_dialog(GtkWindow *parent){
+    GtkBuilder *builder;
+    GtkWidget *dialog, *content_area;
+    GObject *content_box;
     GtkDialogFlags flags;
 
-    // Create the widgets
+    builder = gtk_builder_new();
+    gboolean ui_load_res = gtk_builder_add_from_file(builder, TASK_INIT_DIALOG_UI_FILE_PATH, NULL);
+    if (!ui_load_res) {
+      g_printerr("Error loading UI file\n");
+      return;
+    }
+
     flags = GTK_DIALOG_DESTROY_WITH_PARENT;
-    dialog = gtk_dialog_new_with_buttons ("Message",
-                                          parent,
-                                          flags,
-                                          "_OK",
-                                          GTK_RESPONSE_NONE,
-                                          NULL);
-    content_area = gtk_dialog_get_content_area (GTK_DIALOG (dialog));
-    label = gtk_label_new ("m");
+    dialog = gtk_dialog_new_with_buttons("Task creation",
+                                         parent,
+                                         flags,
+                                         "_OK",
+                                         GTK_RESPONSE_ACCEPT,
+                                         "_Cancel",
+                                         GTK_RESPONSE_REJECT,
+                                         NULL);
+    content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
+    content_box = gtk_builder_get_object(builder, "contentArea");
 
-    // Ensure that the dialog box is destroyed when the user responds
-
-    g_signal_connect_swapped (dialog,
-                              "response",
-                              G_CALLBACK (gtk_window_destroy),
-                              dialog);
-
-    // Add the label, and show everything we’ve added
-
-    gtk_box_append (GTK_BOX (content_area), label);
-    gtk_widget_show (dialog);
-    
-    task = task_init(task_name, task_deadline);
-
-    return task;
+    g_signal_connect(dialog, "response", G_CALLBACK(dialog_response_handler), NULL);
+    g_signal_connect_swapped(dialog,
+                             "response",
+                             G_CALLBACK (gtk_window_destroy),
+                             dialog);
+    gtk_box_append(GTK_BOX(content_area), GTK_WIDGET(content_box));
+    gtk_widget_show(dialog);
 }
--- gui/mainwindow.h
+++ gui/mainwindow.h
@@ -6,5 +6,9 @@ void activate_mainwindow(GtkApplication *app, gpointer user_data);
 void add_task_handler();
 void remove_task_handler();
 
-Task *task_init_dialog(GtkWindow *parent);
-void taskboxlist_show_task(Task *task, GtkWidget *taskboxlist);
+void taskboxlist_show_task(GtkWidget *taskboxlist, Task *task);
+
+void task_init_dialog(GtkWindow *parent);
+void dialog_response_handler(GtkDialog *dialog, gint response_id);
+void dialog_accept_handler(GtkDialog *dialog);
+void dialog_reject_handler(GtkDialog *dialog);
