f1e2a3aa0a9110abda9810dc166ce3009ee12a84
antitoxic336 <73442802+antitoxic316@users.noreply.github.com>
2025-04-25 10:25:20 +0200

    ugly task description

--- gui/tasksboxlist.c
+++ gui/tasksboxlist.c
@@ -3,6 +3,8 @@
 
 #include "path_config.h"
 
+#include <string.h>
+
 void tasksboxlist_show_saved_tasks(GtkWidget *tasksboxlist){
   TaskList *tasks = g_object_get_data(G_OBJECT(tasksboxlist), "tasks");
   if(!tasks){
@@ -17,6 +19,10 @@ void tasksboxlist_show_task(GtkWidget *taskboxlist, Task *task){
   GtkBuilder *builder;
   GtkListBoxRow *row;
   GtkLabel *task_name_label;
+  GtkTextView *task_description_textview;
+  GtkTextBuffer *task_description_textbuffer;
+
+  char *task_description = task_get_description(task);
 
   builder = gtk_builder_new();
   gboolean ui_load_res = gtk_builder_add_from_file(builder, LIST_BOX_ROW_UI_FILE_PATH, NULL);
@@ -26,12 +32,27 @@ void tasksboxlist_show_task(GtkWidget *taskboxlist, Task *task){
   }
 
   row = GTK_LIST_BOX_ROW(gtk_builder_get_object(builder, "tasklistBoxRow"));
+  
   task_name_label = GTK_LABEL(gtk_builder_get_object(builder, "taskName"));
   
+  task_description_textview = GTK_TEXT_VIEW(gtk_builder_get_object(builder, "taskDescription"));
+  task_description_textbuffer = gtk_text_view_get_buffer(task_description_textview);
+  gtk_text_buffer_set_text(
+    task_description_textbuffer,
+    task_description,
+    -1
+  );
+
+  printf("%s\n", task_description);
+  
   g_object_set_data(G_OBJECT(row), "task_id", task->id);
-  gtk_label_set_label(task_name_label, task->name);
+  gtk_label_set_label(task_name_label, task_get_name(task));
   
   gtk_list_box_append(GTK_LIST_BOX(taskboxlist), GTK_WIDGET(row));
+  
+  gtk_widget_set_size_request(GTK_WIDGET(task_description_textview), -1, 50);
+
+
 }
 
 void tasksboxlist_refresh(GtkWidget *tasksboxlist){
